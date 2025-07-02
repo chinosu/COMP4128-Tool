7e3e048dad9242569e4cd351e90a3cbd6d4e4b3f
atri <46995931+Chinosu@users.noreply.github.com>
2025-07-02 21:32:08 +1000

    [`main.cc`] byebye `bss` 🥺

--- src/starterfiles/main.cc
+++ src/starterfiles/main.cc
@@ -15,24 +15,6 @@ using namespace std;
 using namespace __gnu_pbds;
 
 #pragma endregion
-#pragma region bss
-
-// template <size_t _, typename kind> kind _bssval;
-
-// template <size_t id> struct _bss
-// {
-//     template <typename kind> inline static kind &_ref = _bssval<id, kind>;
-
-// template <size_t max_n, typename kind> consteval static auto _arr(size_t n = max_n)
-// {
-//     return span(_bssval<id, array<kind, max_n>>).subspan(0, n);
-// }
-// };
-
-// #define bss_arr _bss<__COUNTER__>::_arr // or __LINE__?
-// #define bss     _bss<__COUNTER__>::_ref
-
-#pragma endregion
 #pragma region macros
 
 #define let                 auto
